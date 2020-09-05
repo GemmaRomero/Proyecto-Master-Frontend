3678a7b08e91bf0a2911d33315ee7eab64417f54
GemmaRomero <gmus121@gmail.com>
2020-09-05 12:49:30 +0200

    previo cambio

--- js/functions.js
+++ js/functions.js
@@ -3,10 +3,10 @@
 function closePopUp(){
     var PopUp = document.getElementById('popUp');
     PopUp.style.visibility = 'hidden';
-    console.log('funciona')
+    
 }
 function openPopUp(){
     var PopUp = document.getElementById('popUp');
     PopUp.style.visibility = 'visible';
-    console.log('funciona')
+    console.log('funciona');
 }
